a8dd7ddfcfe22649c31b7f3b20584c74119cd74a
LeonardoKist <96032875+leokist@users.noreply.github.com>
2023-05-23 16:04:01 -0300

    Atualizado situações para testar

--- combteste.py
+++ combteste.py
@@ -3,8 +3,36 @@ from combustao import *
 
 razao_equiv="1"
 
+#comb2 = Combustao(comb=c2h5oh_L, oxid=o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c2h5oh_L, oxid=ar_G) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c2h5oh_L, oxid=h2o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c2h5oh_L, oxid=hno3_L) #OK RE=1 | RE=?
 
-comb2 = Combustao(comb=c2h8n2_L, oxid=ar_G)
+#comb2 = Combustao(comb=h2_L, oxid=o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=h2_L, oxid=ar_G) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=h2_L, oxid=h2o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=h2_L, oxid=hno3_L) #OK RE=1 | RE=?
+
+#comb2 = Combustao(comb=c8h18_L, oxid=o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c8h18_L, oxid=ar_G) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c8h18_L, oxid=h2o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c8h18_L, oxid=hno3_L) #OK RE=1 | RE=?
+ 
+#comb2 = Combustao(comb=c3h8_L, oxid=o2_L) #OK RE=1 | RE=?
+comb2 = Combustao(comb=c3h8_L, oxid=ar_G) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c3h8_L, oxid=h2o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c3h8_L, oxid=hno3_L) #OK RE=1 | RE=?
+
+#comb2 = Combustao(comb=c2h8n2_L, oxid=o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c2h8n2_L, oxid=ar_G) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c2h8n2_L, oxid=h2o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=c2h8n2_L, oxid=hno3_L) #OK RE=1 | RE=?
+
+#comb2 = Combustao(comb=nh3_L, oxid=o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=nh3_L, oxid=ar_G) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=nh3_L, oxid=h2o2_L) #OK RE=1 | RE=?
+#comb2 = Combustao(comb=nh3_L, oxid=hno3_L)  #OK RE=1 | RE=?
+
 comb2.reacao_combustao(float(razao_equiv))
 print(comb2.reacao_estequiometrica_resultado)
 print(comb2.reacao_dissociacao_resultado)
