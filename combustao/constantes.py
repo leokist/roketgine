13cbbd000618cb8ae68c32c00691de1f13c761ca
LeonardoKist <96032875+leokist@users.noreply.github.com>
2023-05-26 16:04:10 -0300

    Corrigido unidade

--- combustao/constantes.py
+++ combustao/constantes.py
@@ -1,5 +1,5 @@
 class Constantes:
     def const_univ_gases():
-        """Retorna a constante universal dos gases em kJ/kmol*K"""
+        """Retorna a constante universal dos gases em J/mol*K"""
         const_univ_gases = 8.31447
         return const_univ_gases
